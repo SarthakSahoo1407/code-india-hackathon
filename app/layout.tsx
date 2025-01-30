5f07e7d06c4635d6f2a6e7d406d97945b256e33a
SarthakSahoo-1407 <sarthak.sahoo@reverieinc.com>
2025-01-30 23:26:22 +0530

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,17 @@ import type React from "react" // Import React
 const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "Swalekh Developer Portal",
-  description: "Swalekh enables seamless Indic language typing integration in applications. Subscribe to our APIs to enhance multilingual text input across web and mobile platforms.",
-  keywords: "Swalekh, Indic typing, multilingual input, API integration, developer tools, Indian language typing, text input API, regional language SDK, phonetic typing, web development tools",
-  author: "Reverie Language Technologies",
-  ogTitle: "Swalekh Developer Portal - Seamless Indic Language Integration",
-  ogDescription: "Integrate Indic typing easily with Swalekh’s APIs. Enable smooth multilingual input for your applications and enhance user experience.",
-  ogImage: "https://swalekh.reverieinc.com/reverie-horizontal-logo-1x.svg",
+  description:
+    "Swalekh is a powerful web-based tool designed to help developers integrate seamless Indic language typing in their applications...",
+  keywords:
+    "Swalekh, Indic typing, multilingual input, API subscription, developer tools, Indic languages...",
+  authors: [{ name: "Reverie Language Technologies" }],
+  openGraph: {
+    title: "Swalekh Developer Portal - Empower Your Applications with Indic Typing",
+    description:
+      "Swalekh provides easy-to-integrate solutions for Indic typing...",
+    images: ["https://swalekh.reverieinc.com/reverie-horizontal-logo-1x.svg"],
+  },
 };
 
 
